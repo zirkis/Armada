d04f645758e903c1a8d85177792e947786fab167
Benjamin HAOUI <benjamin.haoui@free.fr>
2016-12-17 00:51:53 +0100

    add clock

--- client/app/controllers/dashboard.js
+++ client/app/controllers/dashboard.js
@@ -4,8 +4,6 @@ const {service} = Ember.inject;
 
 export default Ember.Controller.extend({
   session: service('session'),
-  hour: new Date().toLocaleTimeString(),
-  minutes: new Date().getMinutes(),
   actions: {
     logout() {
       this.get('session').invalidate();
