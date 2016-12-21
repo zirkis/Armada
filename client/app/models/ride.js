994f87ed2848ea16eed9e28efa8efe410bde05e5
didelotK <didelotKev@gmail.com>
2016-12-21 23:01:26 +0100

    test google distance

--- client/app/models/ride.js
+++ client/app/models/ride.js
@@ -4,5 +4,6 @@ export default DS.Model.extend({
   vehicleId: DS.belongsTo('vehicle'),
   departurePlace: DS.attr('string'),
   departureTime: DS.attr('date'),
+  travelTime: DS.attr('number'),
   arrivalPlace: DS.attr('string')
 });
